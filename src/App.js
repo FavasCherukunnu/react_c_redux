93fd5b4eb2b9b529f7854bec6f66cee847493e60
Favas Cherukunnu <mhdfavascheru@gmail.com>
2023-09-02 10:58:21 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { store } from './app/store';
 import { TodoListPage } from './pages/todoListPage';
 
 
+//first create store and assign reducer to it
+//the reducer come from slices, the slices has initial state(this is the value stored in storage)
+// states are changing using dispatch.
+// when the dispatch function is called, the selector method is automatically working and the state of component is updating
+
 function App() {
 
   return (
